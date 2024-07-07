f7a594f6afa6e7de9823e3c08c5466fdf7e4a73c
Rowland <rowland.h.mosbergen@gmail.com>
2024-07-07 14:56:16 +1000

    Adding schema

--- main.py
+++ main.py
@@ -1,7 +1,91 @@
 from fastapi import FastAPI
+import sqlite3
 
 # Create an instance of FastAPI
 app = FastAPI()
+
+
+DATABASE = 'data/data_redmane.db'
+
+# Initialize the database and create the tables if they don't exist
+def init_db():
+    conn = sqlite3.connect(DATABASE)
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS projects (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        status TEXT
+    );
+    ''')
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS datasets (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        project_id INTEGER NOT NULL,
+        name TEXT,
+        FOREIGN KEY (project_id) REFERENCES projects(id)
+    );
+    ''')
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS patients (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        project_id INTEGER NOT NULL,
+        ext_patient_id TEXT,
+        ext_patient_url TEXT,
+        public_patient_id TEXT,
+        FOREIGN KEY (project_id) REFERENCES projects(id)
+    );
+    ''')
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS patients_metadata (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        patient_id INTEGER NOT NULL,
+        key TEXT,
+        value TEXT,
+        FOREIGN KEY (patient_id) REFERENCES patients(id)
+    );
+    ''')
+
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS samples (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        patient_id INTEGER NOT NULL,
+        ext_sample_id TEXT,
+        ext_sample_org TEXT,
+        ext_org_batch TEXT,
+        tissue TEXT,
+        sample_date TEXT,
+        internal_batch TEXT,
+        FOREIGN KEY (patient_id) REFERENCES patients(id)
+    );
+    ''')
+
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS samples_metadata (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        sample_id INTEGER NOT NULL,
+        key TEXT,
+        value TEXT,
+        FOREIGN KEY (sample_id) REFERENCES samples(id)
+    );
+    ''')
+
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS raw_file_information (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        dataset_id INTEGER NOT NULL,
+        directory TEXT,
+        sample_id INTEGER,
+        FOREIGN KEY (dataset_id) REFERENCES datasets(id),
+        FOREIGN KEY (sample_id) REFERENCES samples(id)
+    );
+    ''')
+
+    conn.commit()
+    conn.close()
+
+# Call the function to initialize the database
+init_db()
 
 
 # Define a route with a path "/"
